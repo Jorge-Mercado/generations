843b86042b8a85533d74290bb84581b4ee72a38a
Jorge Mercado <mercadoruizjorgearturo@gmail.com>
2022-01-17 12:32:25 -0600

    la feliprueba ya completa

--- feliprueba14enero.js
+++ feliprueba14enero.js
@@ -1,60 +1,49 @@
-var carreritas = ["Lucia","Roberto", "Maria", "Jesus","Andrea", "Jose"]
+var carreritas = ["Lucia", "Roberto", "Maria", "Jesus", "Andrea", "Jose"];
 
-function competicion(){
+function competicion() {
+  carreritas.forEach(function (element, index, array) {
+    console.log("posicion " + (index + 1) + ", " + element);
+  });
 
-    carreritas.forEach(function(element,index,array) {
-        
+  console.log("la carrera continua asi: ");
 
-console.log("posicion " + (index + 1 ) + ", " + element);
+  var indice = carreritas.indexOf("Maria");
+  carreritas[indice] = "Andrea";
+  carreritas[3] = "Maria";
+  carreritas[4] = "Jesus";
 
-    });
+  console.log("andrea adelanta a maria: ");
+  carreritas.forEach(function (element, index, array) {
+    console.log("posicion " + (index + 1) + ", " + element);
+  });
+  console.log("Jose es descalificado:");
 
+  carreritas.pop();
+  carreritas.forEach(function (element, index, array) {
+    console.log("posicion " + (index + 1) + ", " + element);
+  });
 
-    console.log("la carrera continua asi: ");
+  console.log(
+    "detras  de lucia y antes de roberto se clasifican 3 nuevos corredores"
+  );
+  console.log("Cristobal ", "Fernanda", " y Armando");
+  carreritas.shift();
 
-    var indice = carreritas.indexOf("Maria");
-    carreritas[indice]= "Andrea";
-    carreritas[3] = "Maria";
-    carreritas[4] = "Jesus";
-    
+  carreritas.unshift("Armando");
+  carreritas.unshift("Fernanda");
+  carreritas.unshift("Cristobal");
+  carreritas.unshift("Lucia");
 
-    console.log("andrea adelanta a maria: ");
-    carreritas.forEach(function(element,index,array) {
-        
+  carreritas.forEach(function (element, index, array) {
+    console.log("posicion " + (index + 1) + ", " + element);
+  });
+  console.log("un nuevo concursante toma el primer puesto, se llama Federico");
 
-        console.log("posicion " + (index + 1 ) + ", " + element);
-        
-            });
-    console.log("Jose es descalificado:");
+  carreritas.unshift("Federico");
 
-    carreritas.pop();
-    carreritas.forEach(function(element,index,array) {
-        
-
-        console.log("posicion " + (index + 1 ) + ", " + element);
-        
-            });
-
-    console.log("detras  de lucia y antes de roberto se clasifican 3 nuevos corredores");
-    console.log("Cristobal ","Fernanda"," y Armando");        
-        carreritas.shift();    
-
-        carreritas.unshift("Armando");
-        carreritas.unshift("Fernanda");
-        carreritas.unshift("Cristobal");
-        carreritas.unshift("Lucia");
-
-        carreritas.forEach(function(element,index,array) {
-        
-
-            console.log("posicion " + (index + 1 ) + ", " + element);
-            
-                });
-                
-
-
-
+  carreritas.forEach(function (element, index, array) {
+    console.log("posicion " + (index + 1) + ", " + element);
+  });
 }
-
 
 competicion();
