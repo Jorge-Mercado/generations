96dd608b6a8ddffed1fbecfa6a673bdc94af2344
Jorge Mercado <mercadoruizjorgearturo@gmail.com>
2022-01-17 09:53:26 -0600

    segundo adelanto de la feliprueba

--- feliprueba14enero.js
+++ feliprueba14enero.js
@@ -11,9 +11,20 @@ console.log("posicion " + (index + 1 ) + ", " + element);
 
 
     console.log("la carrera continua asi: ");
+
+    var indice = carreritas.indexOf("Maria");
+    carreritas[indice]= "Andrea";
+    carreritas[3] = "Maria";
+    carreritas[4] = "Jesus";
     
 
+    console.log("andrea adelanta a maria: ");
+    carreritas.forEach(function(element,index,array) {
+        
 
+        console.log("posicion " + (index + 1 ) + ", " + element);
+        
+            });
 
 
 }
