6e65580515be70939158c674d87255609c8fae88
Jorge Mercado <mercadoruizjorgearturo@gmail.com>
2022-01-17 00:59:50 -0600

    primer adelanto de la feliprueba de arrays

---
+++ feliprueba14enero.js
@@ -0,0 +1,22 @@
+var carreritas = ["Lucia","Roberto", "Maria", "Jesus","Andrea", "Jose"]
+
+function competicion(){
+
+    carreritas.forEach(function(element,index,array) {
+        
+
+console.log("posicion " + (index + 1 ) + ", " + element);
+
+    });
+
+
+    console.log("la carrera continua asi: ");
+    
+
+
+
+
+}
+
+
+competicion();
